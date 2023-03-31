eb3b29bdd991423b322f1df009cd9943cdc1dfb3
Aigul Ibrakova <aigul.ibrakova@gmail.com>
2023-03-31 21:07:34 +0300

    add line with branch1

--- Gittest1/Program.cs
+++ Gittest1/Program.cs
@@ -8,7 +8,7 @@ namespace MyApp // Note: actual namespace depends on the project name.
         {
             Console.WriteLine("Hello World!");
             var name = Console.Readline();
-            Console.Writeline(name)
+            Console.Writeline("This is branch1");
         }
     }
 }
