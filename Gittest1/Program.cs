ab4005bc39386bb4a720e2bcbb2f358f9f9bc64a
Aigul Ibrakova <aigul.ibrakova@gmail.com>
2023-08-21 01:01:29 +0300

    add Home2

--- Gittest1/Program.cs
+++ Gittest1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data;
 
 namespace MyApp // Note: actual namespace depends on the project name.
 {
@@ -9,6 +10,50 @@ namespace MyApp // Note: actual namespace depends on the project name.
             
             var name = Console.ReadLine();
             Console.WriteLine($"Hello {name}") ;
+            Task1();
+            
+        }
+
+        public static void Task1()
+         {
+            double radius = Convert.ToDouble(Console.ReadLine());
+            double height = Convert.ToDouble(Console.ReadLine());
+            double pi = Math.PI;
+
+            double volume = pi * Math.Pow(radius, 2) * height;
+            Console.WriteLine("Volume:" + volume);
+            
+        }
+        /*Напишите программу определения, попадает ли указанное пользователем число 
+         * от 0 до 100 в числовой промежуток [0 - 14] [15 - 35] [36 - 50][50 - 100].
+Если да, то укажите, в какой именно промежуток.
+Если пользователь указывает число, не входящее ни в один из имеющихся числовых промежутков, то выводится соответствующее сообщение.*/
+
+        public static void Task2()
+        {
+            string numberStr = Console.ReadLine();
+            int number = int.Parse(numberStr);
+
+            if (( number >= 0 ) && (number <= 14))
+            {
+                Console.WriteLine("Число входит в отрезок 0 - 14");
+            }
+            else if ((number >= 15) && (number <= 35))
+            {
+                Console.WriteLine("Число входит в отрезок 15 - 35");
+            }
+            else if ((number >= 36) && (number <= 50))
+            {
+                Console.WriteLine("Число входит в отрезок 36 - 50");
+            }
+            else if ((number >= 51) && (number <= 100))
+            {
+                Console.WriteLine("Число входит в отрезок 51- 100");
+            }
+            else
+            {
+                Сonsole.WriteLine("Число не входит в начальные условия");
+            }
         }
     }
 }
---
+++ Home2/Program.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlTypes;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Task1();
+            Task2();
+            Task3();
+            Task4();
+        }
+
+        public static void Task1()
+        {
+            Console.Write("Первое число ");
+            string input1 = Console.ReadLine();
+            int operand1 = Int32.Parse(input1);
+            Console.Write("Второе число число ");
+            string input2 = Console.ReadLine();
+            int operand2 = Int32.Parse(input2);
+            Console.Write("Знак операции ");
+            string sign = Console.ReadLine();
+
+            switch (sign)
+            {
+                case "+":
+                    {
+                        Console.WriteLine(operand1 + operand2);
+                        break;
+                    }
+
+                case "-":
+                    {
+                        Console.WriteLine(operand1 - operand2);
+                        break;
+                    }
+
+                case "*":
+                    {
+                        Console.WriteLine(operand1 * operand2);
+                        break;
+                    }
+
+                case "/":
+                    {
+                        try
+                        {
+                            Console.WriteLine(operand1 / operand2);
+                        }
+                        catch (DivideByZeroException)
+                        { Console.WriteLine("Деление на 0!"); }
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Некорректный знак");
+                        break;
+                    }
+
+            }
+            
+
+        }
+    
+        public static void Task2()
+        {
+            Console.Write("Введите число ");
+            string numberStr = Console.ReadLine();
+            
+            int number = int.Parse(numberStr);
+
+            if ((number >= 0) && (number <= 14))
+            {
+                Console.WriteLine("Number in 0 - 14");
+                return;
+            }
+            else if ((number >= 15) && (number <= 35))
+            {
+                Console.WriteLine("Number in 15 - 35");
+                return;
+            }
+            else if ((number >= 36) && (number <= 50))
+            {
+                Console.WriteLine("Number in 36 - 50");
+                return;
+            }
+            else if ((number >= 51) && (number <= 100))
+            {
+                Console.WriteLine("Number in 51- 100");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Number error");
+                
+            }
+        }
+        public static void Task3()
+        /*Напишите программу русско-английский переводчик.
+        Программа знает 10 слов о погоде.
+        Требуется, чтобы пользователь вводил слово на русском языке, а программа давала ему перевод этого слова на английском языке.
+        Если пользователь ввел слово, для которого отсутствует перевод, то следует вывести сообщение, что такого слова нет.
+        */
+
+        {
+            {
+                Console.Write("Введите слово ");
+                String input = Console.ReadLine();
+                String Weather = input.ToLower();
+                
+
+                if (Weather == "жарко")
+                {
+                    Console.WriteLine("Hot");
+                    return;
+                }
+                else if (Weather == "тепло")
+                {
+                    Console.WriteLine("Warm");
+                    return;
+                }
+                else if (Weather == "холодно")
+                {
+                    Console.WriteLine("Cold");
+                    return;
+                }
+                else if (Weather == "солнечно")
+                {
+                    Console.WriteLine("Sunny");
+                    return;
+                }
+                else if (Weather == "пасмурно")
+                {
+                    Console.WriteLine("Cloudy");
+                    return;
+                }
+                else if (Weather == "дождливо")
+                {
+                    Console.WriteLine("Raining");
+                    return;
+                }
+                else if (Weather == "ветрено")
+                {
+                    Console.WriteLine("Windy");
+                    return;
+                }
+                else if (Weather == "мрачно")
+                {
+                    Console.WriteLine("Gloomy");
+                    return;
+                }
+                else if (Weather == "ярко")
+                {
+                    Console.WriteLine("Bright");
+                    return;
+                }
+                else if (Weather == "безоблачно")
+                {
+                    Console.WriteLine("Clear");
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("Not found");
+                }
+            }
+        }
+        public static void Task4()
+        /*Напишите программу, которая будет выполнять проверку чисел на четность.
+        */
+
+        {
+            Console.Write("Введите число ");
+            int number3= int.Parse(Console.ReadLine());
+
+            if ((number3 % 2) == 0)
+            {
+                Console.WriteLine("Число четное");
+            }
+            else
+            {
+                Console.WriteLine("Число нечетное");
+            }
+        }
+    }
+}
